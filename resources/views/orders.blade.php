e6fc87576fed9fbbcd1b5fd80ff2e40251f44f71
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-08 06:54:30 +0700

    Fix empty order message not showing caused by incorrect logic

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,7 +1,7 @@
 <x-customer-layout>
     @if (count($orders) > 0)
         <ul>
-            @forelse ($orders as $order)
+            @foreach ($orders as $order)
                 <li>
                     <p>
                         <a href="/order/{{ $order->id }}">Nomor pesanan: {{ $order->id }}</a>
@@ -9,9 +9,9 @@
                     <p>Nama: {{ $order->customer_name }}</p>
                     <p>Status: {{ $order->status }}</p>
                 </li>
-            @empty
-                <p>Kamu belum pesan apapun</p>
-            @endforelse
+            @endforeach
         </ul>
+    @else
+        <p>Kamu belum pesan apapun</p>
     @endif
 </x-customer-layout>
