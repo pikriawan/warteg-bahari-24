ac88799df0b841422ba21d1197faef13d49db926
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-15 19:35:09 +0700

    Add menu quantity counter

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,17 @@
+@php
+    use App\Models\Customer;
+
+    $customer = Customer::where('session_id', session()->getId())->first();
+    $cart = $customer
+        ->orders
+        ->where('status', null)
+        ->first();
+    $menus = $cart->menus;
+
+    $totalQuantity = $menus->reduce(function (?float $carry, $menu) {
+        return $carry + $menu->pivot->menu_quantity;
+    });
+@endphp
 @push('styles')
     @vite('resources/css/header.css')
 @endpush
@@ -11,20 +25,29 @@
     <a href="/">
         <img src="/images/warteg-bahari-24.svg" alt="Warteg Bahari 24" width="128">
     </a>
-    <div class="mobile-navbar-toggle-balancer"></div>
+    <div class="mobile-navbar-toggle-container">
+        <a class="button button-primary cart-link" href="/cart">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-basket-icon lucide-shopping-basket"><path d="m15 11-1 9"/><path d="m19 11-4-7"/><path d="M2 11h20"/><path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4"/><path d="M4.5 15.5h15"/><path d="m5 11 4-7"/><path d="m9 11 1 9"/></svg>
+            @if ($totalQuantity !== null)
+                <span class="menu-quantity-counter">{{ $totalQuantity }}</span>
+            @endif
+        </a>
+    </div>
     <nav class="mobile-navbar" id="mobileNavbar">
         <a class="navbar-link" href="/">Beranda</a>
         <a class="navbar-link" href="/menus">Menu</a>
         <a class="navbar-link" href="/orders">Pesanan</a>
-        <a class="navbar-link" href="/cart">Keranjang</a>
     </nav>
     <nav class="desktop-navbar">
         <a class="navbar-link" href="/">Beranda</a>
         <a class="navbar-link" href="/menus">Menu</a>
         <a class="navbar-link" href="/orders">Pesanan</a>
-        <a class="button button-primary" href="/cart">
+        <a class="button button-primary cart-link" href="/cart">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-basket-icon lucide-shopping-basket"><path d="m15 11-1 9"/><path d="m19 11-4-7"/><path d="M2 11h20"/><path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4"/><path d="M4.5 15.5h15"/><path d="m5 11 4-7"/><path d="m9 11 1 9"/></svg>
             Keranjang
+            @if ($totalQuantity !== null)
+                <span class="menu-quantity-counter">{{ $totalQuantity }}</span>
+            @endif
         </a>
     </nav>
 </header>
