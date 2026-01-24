8633987d34b7c1cb47275edfcb26341e6332bf21
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-24 22:31:55 +0700

    Refactor admin dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
         $totalRevenue = Order::where('status', 'finished')->get()->reduce(function (?float $carry, $order) {
             return $carry + $order->menus->reduce(function (?float $carry, $menu) {
                 return $carry + $menu->price * $menu->pivot->menu_quantity;
-            });
+            }, 0);
         }, 0);
 
         $totalSales = Order::where('status', 'finished')->count();
