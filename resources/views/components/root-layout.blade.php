7b258b6475a18089590bb1f11e432945f1080817
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-01-31 19:06:36 +0700

    Change website title

--- resources/views/components/root-layout.blade.php
+++ resources/views/components/root-layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>WarBar 24</title>
+        <title>Warteg Bahari 24</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
